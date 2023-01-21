7aed04c3dcb63d1bc5fffd6b45cfb80dce316352
Avi Duda <368677+AviDuda@users.noreply.github.com>
2023-01-21 22:33:49 +0100

    Add raccoons 🦝

--- apps/first/src/pages/index.tsx
+++ apps/first/src/pages/index.tsx
@@ -1,11 +1,14 @@
 import type { PageProps } from "gatsby";
+import { Link } from "gatsby";
 import React from "react";
 
-const IndexPage = (props: PageProps) => (
+const IndexPage = (_props: PageProps) => (
   <main>
     <h2>Howdy!</h2>
     <p>App: {process.env.GATSBY_SUBDOMAIN}</p>
-    <p>Port: {props.location.port}</p>
+    <p>
+      <Link to="raccoons">Go see some raccoons</Link>
+    </p>
   </main>
 );
 
---
+++ apps/first/src/pages/raccoons.tsx
@@ -0,0 +1,15 @@
+import type { PageProps } from "gatsby";
+import { Link } from "gatsby";
+import React from "react";
+
+const RaccoonsPage = (_props: PageProps) => (
+  <main>
+    <h2>Raccoons</h2>
+    <p>🦝🦝🦝🦝🦝🦝🦝🦝🦝🦝🦝🦝🦝🦝🦝🦝🦝🦝🦝</p>
+    <p>
+      <Link to="/">Go back</Link>
+    </p>
+  </main>
+);
+
+export default RaccoonsPage;
