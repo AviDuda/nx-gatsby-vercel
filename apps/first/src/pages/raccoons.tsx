0d1e146b0a96a1d27ee3b4ea2078252a0ba7c13f
Avi Duda <368677+AviDuda@users.noreply.github.com>
2023-01-21 22:53:03 +0100

    Animated raccoons!!

--- apps/first/src/pages/raccoons.tsx
+++ apps/first/src/pages/raccoons.tsx
@@ -1,15 +1,32 @@
 import type { PageProps } from "gatsby";
 import { Link } from "gatsby";
 import React from "react";
+import styled, { keyframes } from "styled-components";
 
 const RaccoonsPage = (_props: PageProps) => (
   <main>
     <h2>Raccoons</h2>
-    <p>🦝🦝🦝🦝🦝🦝🦝🦝🦝🦝🦝🦝🦝🦝🦝🦝🦝🦝🦝</p>
-    <p>
-      <Link to="/">Go back</Link>
-    </p>
+    <Link to="/">Go back</Link>
+    <Raccoons aria-hidden>
+      {new Array(1000).fill(null).map((_, i) => (
+        <span key={i}>🦝</span>
+      ))}
+    </Raccoons>
   </main>
 );
+
+const RaccoonFlip = keyframes`
+  0% { transform: matrix(0, 0, 0) };
+  50% { transform: matrix(-1, 0, 0, 1, 0, 0); };
+  100% { transform: matrix(0, 0, 0) };
+`;
+
+const Raccoons = styled("section")`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 1rem;
+  font-size: 10rem;
+  animation: ${RaccoonFlip} 5s linear infinite;
+`;
 
 export default RaccoonsPage;
