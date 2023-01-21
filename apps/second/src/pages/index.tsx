3aeeb16fe3d2f1a57d39ffde13f4c916a430d587
Avi Duda <368677+AviDuda@users.noreply.github.com>
2023-01-21 23:12:05 +0100

    Add raccoon hint to second app

--- apps/second/src/pages/index.tsx
+++ apps/second/src/pages/index.tsx
@@ -6,6 +6,7 @@ const IndexPage = (props: PageProps) => (
     <h2>Howdy!</h2>
     <p>App: {process.env.GATSBY_SUBDOMAIN}</p>
     <p>Port: {props.location.port}</p>
+    <p>Hint: There may raccoons on different deployments.</p>
   </main>
 );
 
